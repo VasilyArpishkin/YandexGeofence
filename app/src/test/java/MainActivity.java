25566c34f63984858c898bf84c8c02406037a12d
varpi <v.arpishkin@yandex.ru>
2024-11-10 00:13:16 +0500

    Initial commit

---
+++ app/src/test/java/MainActivity.java
@@ -0,0 +1,19 @@
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.yandexgeofence.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
